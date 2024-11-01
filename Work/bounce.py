a1e308777baeb6b4b41cefe6188ebdbcc03f29db
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2024-11-01 15:16:48 +1300

    Complete Exercise 1.5: The Bouncing Ball

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+original_height = 100
+count = 1
+while count < 11:
+    jump_back_height = 3/5 * original_height
+    original_height = jump_back_height
+    print(round(original_height, 1))
+    count = count + 1
