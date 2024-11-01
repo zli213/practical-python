86e215b16eedacf4f2c488a8501dc97f683a1f7e
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2024-11-01 16:38:18 +1300

    Exercise 1.9: Making an Extra Payment Calculator

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,11 +6,13 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 totalMonth = 0
-
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
 while principal > 0:
-    if totalMonth < 12:
-        principal = principal * (1+rate/12) - (payment + 1000)
-        total_paid = total_paid + (payment + 1000)
+    if totalMonth <= extra_payment_end_month and totalMonth >= extra_payment_start_month:
+        principal = principal * (1+rate/12) - (payment + extra_payment)
+        total_paid = total_paid + (payment + extra_payment)
     else:
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
