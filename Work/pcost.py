bb126aede84ad58708304ae04616f247a3579360
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2024-11-04 17:34:49 +1300

    Exercise 1.28: Other kinds of “files”

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,7 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
-with open('Data/portfolio.csv', 'rt') as f:
+# with open('Data/portfolio.csv', 'rt') as f:
+#     next(f)
+#     sum = 0
+#     for line in f:
+#         row = line.split(',')
+#         sum += int(row[1]) * float(row[2])
+# print(f'Total cost {sum}')
+import gzip
+with gzip.open('Data/portfolio.csv.gz', 'rt') as f:
     next(f)
     sum = 0
     for line in f:
